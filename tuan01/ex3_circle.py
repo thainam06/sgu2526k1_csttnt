0207ab6558a763e16eecf8e22edbbabb4f94e720
thainam06 <boydyt2@gmail.com>
2026-01-12 19:56:32 +0700

    Add files via upload

---
+++ tuan01/ex3_circle.py
@@ -0,0 +1,7 @@
+import math
+print("Calculator Basic")
+r = float(input("Moi ban nhap ban kinh : "))
+chu_vi = 2 * math.pi * r
+dien_tich = math.pi * r * r
+print(" Chu vi hinh tron la : ", chu_vi)
+print("dien tich hinh tron la : ",dien_tich)
